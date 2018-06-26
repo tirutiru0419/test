bf1bca8481c0772cb421dc1a0818c44c6f94c51a
kobayashi <zero.abcmart0823@gmail.com>
2018-06-26 15:57:53 +0900

    second commit

---
+++ Bookmarks/EntranceAndExitRecord.cs
@@ -0,0 +1,33 @@
+﻿using EntranceAndExitRecord.Entity;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+
+namespace EntranceAndExitRecord.Model
+{
+    public class EntranceAndExitRecordModel : DbContext
+    {
+        public DbSet<Item> Items { get; internal set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var connectionString = new SqliteConnectionStringBuilder { DataSource = @"C:\work\EntranceAndExitRecord.db" }.ToString();
+            optionsBuilder.UseSqlite(new SqliteConnection(connectionString));
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // 複合キーの場合、以下のように指定してやる。
+            // ない場合「Entity type 'Item' has composite primary key defined with data annotations. To set composite primary key, use fluent API.」と表示される
+            modelBuilder.Entity<Item>().HasKey(c => new { c.Id, c.PointNo });
+        }
+    }
+
+    public class Book
+    {
+        public int UserId { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Password { get; set; }
+    }
+
+}
---
+++ Bookmarks/MainPage.xaml.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.System;
+
+// 空白ページの項目テンプレートについては、https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x411 を参照してください
+
+namespace Bookmarks
+{
+    /// <summary>
+    /// それ自体で使用できる空白ページまたはフレーム内に移動できる空白ページ。
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            ContentFrame.Navigate(typeof(home));
+
+            string appName = Windows.ApplicationModel.Package.Current.DisplayName;
+
+            //NavView.MenuItemsSource = new NavViewItems();
+
+            double newWidth = 2;
+            double newHeight = 3;
+            bool result
+              = Windows.UI.ViewManagement.ApplicationView.GetForCurrentView()
+                .TryResizeView(new Size { Width = newWidth, Height = newHeight });
+        }
+
+        //押されたアイテムの処理
+        private void NavView_SelectionChanged(NavigationView sender, NavigationViewSelectionChangedEventArgs args)
+        {
+            if (args.IsSettingsSelected)
+            {
+                //ContentFrame.Navigate(typeof(SettingsPage));
+            }
+            else
+            {
+                NavigationViewItem item = args.SelectedItem as NavigationViewItem;
+
+                switch (item.Tag.ToString())
+                {
+                    case "home":
+                        ContentFrame.Navigate(typeof(home));
+                        NavView.Header = "ホーム";
+                        break;
+
+                    case "signup":
+                        ContentFrame.Navigate(typeof(signin));
+                        NavView.Header = "サインアップ";
+                        break;
+
+                    case "signin":
+                        ContentFrame.Navigate(typeof(Signup));
+                        NavView.Header = "サインイン";
+                        break;
+
+                    case "signout":
+                        ContentFrame.Navigate(typeof(Page));
+                        NavView.Header = "サインアウト";
+                        break;
+
+                    case "work":
+                        ContentFrame.Navigate(typeof (Page1));
+
+                        //確認用
+                        signin.Visibility = Visibility.Collapsed;
+                        signup.Visibility = Visibility.Collapsed;
+                        signout.Visibility = Visibility.Visible;
+
+                        NavView.Header = "仕事";
+                        break;
+
+                    case "music":
+                        ContentFrame.Navigate(typeof(Page1));
+                        NavView.Header = "音楽";
+                        break;
+
+                    case "enter":
+                        ContentFrame.Navigate(typeof(Page1));
+                        NavView.Header = "芸能";
+                        break;
+
+                    case "sport":
+                        ContentFrame.Navigate(typeof(Page1));
+                        NavView.Header = "スポーツ";
+                        break;
+
+                    case "fashion":
+                        ContentFrame.Navigate(typeof(Page1));
+                        NavView.Header = "ファッション";
+                        break;
+
+                    case "game":
+                        ContentFrame.Navigate(typeof(Page1));
+                        NavView.Header = "ゲーム";
+                        break;
+
+                    case "other":
+                        ContentFrame.Navigate(typeof(Page1));
+                        NavView.Header = "その他";
+                        break;
+                }
+            }
+        }
+    }
+}
